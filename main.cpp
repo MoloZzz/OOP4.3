aaf3fbd9af9ff82c45bb38ef020de4c12eb78295
MoloZzz <mega_oleksii@ukr.net>
2023-05-27 14:58:00 +0300

    +newThread
    +add new thread and tested time

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include"comlexInt.h"
 #include "B-treeClass.h"
+
+
+
+#include <ctime>
+
+#include <random> //random
+
+#include <stdio.h>
+#include <time.h>
+
+
+#include <thread> //potoki
+
 using namespace std;
 
 
@@ -10,7 +23,13 @@ using namespace std;
 
 
 
+int randomInt(int min, int max)
+{
+    std::random_device rd;
+    std::mt19937 mersenne(rd());
 
+    return mersenne()%max;
+}
 
 
 void createTreeAndCheck(){
@@ -29,17 +48,40 @@ void createTreeAndCheck(){
     t.insert(Complex(17, 27));
     t.insert(Complex(55, 22));
 
-    cout << "B-tree: ";
-
     t.traverse();
 
     Complex k = Complex(10, 2);
 
     (t.search(k) != NULL)
-    ? cout << endl << k.getString() << " find"
-    : cout << endl << k.getString() << " not find";
+    ? cout << endl << k.getString() << " find" << endl
+    : cout << endl << k.getString() << " not find" << endl;
 
     Complex m = Complex(55, 22);
+
+    (t.search(m) != NULL)
+    ? cout << endl << m.getString() << " find" << endl
+    : cout << endl << m.getString() << " not find\n" << endl;
+}
+
+void createRandomTreeAndCheck(){
+    BTree t(3);
+
+    int max = 99;
+    int length = 1000;
+
+    for(int i = length; i > 0 ; i--) {
+        t.insert(Complex(randomInt(1,max), randomInt(1,max)));
+    }
+
+    //t.traverse();
+
+    Complex k = Complex(randomInt(1,max), randomInt(1,max));
+
+    (t.search(k) != NULL)
+    ? cout << endl << k.getString() << " find"
+    : cout << endl << k.getString() << " not find\n";
+
+    Complex m = Complex(randomInt(1,max), randomInt(1,max));
 
     (t.search(m) != NULL)
     ? cout << endl << m.getString() << " find"
@@ -47,9 +89,24 @@ void createTreeAndCheck(){
 }
 
 
+
 int main() {
 
     setlocale(LC_ALL, "Ukrainian");
 
-   createTreeAndCheck();
+    clock_t start = clock();
+
+     thread newThread(createRandomTreeAndCheck);
+
+    createRandomTreeAndCheck();
+
+    newThread.join();
+
+    clock_t end = clock();
+    double seconds = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("The time: %f seconds\n", seconds);
+
+
+
+
 }
